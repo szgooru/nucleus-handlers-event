6dd4d9f43029dc0be53c74fe7473d58d1d71b7b1
Ashish Mishra <ashish@gooru.org>
2015-12-25 11:34:47 +0530

    Initial repo creation

---
+++ src/main/java/org/gooru/nucleus/handlers/events/bootstrap/EventVerticle.java
@@ -0,0 +1,9 @@
+package org.gooru.nucleus.handlers.events.bootstrap;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * Created by ashish on 25/12/15.
+ */
+public class EventVerticle extends AbstractVerticle {
+}
